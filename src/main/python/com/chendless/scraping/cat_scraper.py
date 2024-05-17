20243480d2c70ffa86f15f96ac73078af8899e51
S1thK3nny <kennethildebrandt@gmail.com>
2024-05-17 18:09:49 +0200

    Modified gitignore, added mongodb saving

--- src/main/python/com/chendless/scraping/cat_scraper.py
+++ src/main/python/com/chendless/scraping/cat_scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from urllib.request import Request, urlopen
 from PIL import Image
+from mongosave import store_page
 import os
 import time
 
@@ -30,42 +31,21 @@ def scroll_to_bottom(driver):
             break
         last_height = new_height
 
-def save_info(info_string, prefix, download_folder):
-    if not os.path.exists(download_folder):
-        os.makedirs(download_folder)
-
-    txt_file_path = os.path.join(download_folder, prefix + '.txt')
-
-    with open(txt_file_path, 'w', encoding='utf-8') as file:
-        file.write(info_string)
-
-    print(f"Daten abgespeichert in: {txt_file_path}")
-
-
-
-def extract_information(driver: webdriver, classname: str, prefix: str, download_folder: str):
+def extract_information(driver: webdriver, classname: str):
     elements = driver.find_elements(By.CLASS_NAME, classname)
     info = [element.text for element in elements]
-    info_string = '\n'.join(info)
-    save_info(info_string, prefix, download_folder)
     return info
 
-
-
 # Use a specific function to add spans together
-def extract_price(driver: webdriver, xpath_price: str, prefix: str, download_folder: str):
+def extract_price(driver: webdriver, xpath_price: str):
     elements = driver.find_elements(By.XPATH, xpath_price)
     prices = []
     for element in elements:
         spans = element.find_elements(By.XPATH, './span')
         price = ''.join([span.text for span in spans])
         prices.append(price)
-    
-    price_string = '\n'.join(prices)
-    save_info(price_string, prefix, download_folder)
     return prices
 
-# Use an XPath instead of classname, faster and more reliable
 def extract_image_urls(driver, xpath):
     try:
         WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
@@ -76,31 +56,6 @@ def extract_image_urls(driver, xpath):
         print(f"Fehler beim Extrahieren der Bild-URLs: {type(e).__name__}, {e}")
         return []
 
-def download_image(image_url, save_path):
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        request = Request(image_url, headers=headers)
-        response = urlopen(request)
-        if response.status == 200:
-            image = Image.open(response)
-            image.save(save_path)
-            print("Speichere Bild: " + save_path)
-        else:
-            print(f"Fehler beim Herunterladen des Bildes: HTTP {response.status}")
-    except Exception as e:
-        print(f"Fehler beim Herunterladen des Bildes: {type(e).__name__}, {e}")
-
-def download_all_images(driver: webdriver, xpath: str, download_folder: str):
-    image_urls = extract_image_urls(driver, xpath)
-    if not os.path.exists(download_folder):
-        os.makedirs(download_folder)
-
-    for idx, image_url in enumerate(image_urls):
-        save_path = os.path.join(download_folder, f'image_{idx + 1}.jpg')
-        download_image(image_url, save_path)
-
-    return image_urls
-
 def main():
     url = 'https://de.aliexpress.com/w/wholesale-Flache-sandalen.html?spm=a2g0o.categorymp.0.0.1912FyQxFyQxUQ&categoryUrlParams=%7B%22q%22%3A%22Flache%20sandalen%22%2C%22s%22%3A%22qp_nw%22%2C%22osf%22%3A%22category_navigate%22%2C%22sg_search_params%22%3A%22%22%2C%22guide_trace%22%3A%22a419bdaa-f1f0-43d9-849b-f06f05004ce9%22%2C%22scene_id%22%3A%2237749%22%2C%22searchBizScene%22%3A%22openSearch%22%2C%22recog_lang%22%3A%22de%22%2C%22bizScene%22%3A%22category_navigate%22%2C%22guideModule%22%3A%22category_navigate_vertical%22%2C%22postCatIds%22%3A%22322%2C201768104%22%2C%22scene%22%3A%22category_navigate%22%7D&isFromCategory=y'
     classname_sold = 'multi--trade--Ktbl2jB'
@@ -108,27 +63,24 @@ def main():
     # classname_image = 'multi--img--1IH3lZb product-img'
     xpath_image = '//*[@id="card-list"]//div/a/div[1]/img'
     xpath_price = '//*[@id="card-list"]/div/div/div/a/div[2]/div[3]/div[1]'
-    download_folder = "C:\\Users\\Kenny\\Documents\\GitHub\\CHendLess\\files"
-    # download_folder = "/Users/dennisschielke/Desktop/Uni/6thSemester/Gründungsmangement/images"
     print("INIT")
 
     driver = init_driver(True)
     try:
         navigate_to_category(driver, url)
-        scroll_to_bottom(driver)
-        sold = extract_information(driver, classname_sold, "Verkauft", download_folder)
-        name = extract_information(driver, classname_name, "Name", download_folder)
-        price = extract_price(driver, xpath_price, "Preis", download_folder)
+        # scroll_to_bottom(driver)
+        sold = extract_information(driver, classname_sold)
+        name = extract_information(driver, classname_name)
+        price = extract_price(driver, xpath_price)
         print("Extrahierte Verkäufe:", sold)
         print("Extrahierte Namen:", name)
         print("Extrahierte Preise: ", price)
-        # Es fehlt immer das letzte Bild. Scroll issue?
-        images = download_all_images(driver, xpath_image, download_folder)
+        image_urls = extract_image_urls(driver, xpath_image)
 
-        if not (len(images) == len(sold) == len(name) == len(price)):
+        if not (len(image_urls) == len(sold) == len(name) == len(price)):
             print("!! Size of stored data is NOT equal !!\nCheck if the window size is correct!")
             return
-        
+        store_page(sold=sold, name=name, price=price, image_urls=image_urls)
         
     finally:
         driver.quit()
--- src/main/python/com/chendless/scraping/mongosave.py
+++ src/main/python/com/chendless/scraping/mongosave.py
@@ -1,15 +1,32 @@
 import requests
 from pymongo import MongoClient
+from bson.binary import Binary
+from PIL import Image
+from io import BytesIO
 
-def scrape_and_store():
-    # Perform web scraping
-    scraped_data = {'example_key': 'example_value'}
-    
-    # Connect to MongoDB and store data
+def resize_and_save_image(image_url):
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
+    image = image.resize((128, 128))
+    byte_arr = BytesIO()
+    image.save(byte_arr, format='PNG')
+    byte_arr = byte_arr.getvalue()
+
+    image_data = Binary(byte_arr)
+    return image_data
+
+# The function assumes that the arrays have the same length. Please run this ONLY once this has been ensured
+def store_page(sold, name, price, image_urls):
     client = MongoClient('mongodb://localhost:27017/')
-    db = client['mydatabase']
-    collection = db['mycollection']
-    collection.insert_one(scraped_data)
+    db = client['AliExpress']
+    collection = db['Test']
 
-if __name__ == "__main__":
-    scrape_and_store()
+    for i in range(len(sold)):
+
+        document = {
+            'Name': name[i],
+            'Price': price[i],
+            'Amount sold': sold[i],
+            'Image': resize_and_save_image(image_urls[i])
+        }
+        collection.insert_one(document)
