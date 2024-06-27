db663cbf8759d70d932c9bbb97d6d1166982c723
S1thK3nny <kennethildebrandt@gmail.com>
2024-06-27 17:05:04 +0200

    Last improvement for the stepcomponent right now

--- frontend/src/components/StepComponent.vue
+++ frontend/src/components/StepComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="[
-      'flex items-center flex-row box-border justify-between xl:px-24 p-10 border-2 my-7 md:my-10 lg:my-20 mx-7 md:mx-10 lg:mx-20 xl:mx-32 2xl:mx-40 rounded-3xl border-gray-400 h-72 md:h-80 lg:h-96',
+      'flex items-center flex-row box-border justify-between xl:px-24 p-3 sm:p-5 lg:p-10 border-2 my-7 md:my-10 lg:my-20 mx-7 md:mx-10 lg:mx-20 xl:mx-32 2xl:mx-40 rounded-3xl border-gray-400 h-72 md:h-80 lg:h-96',
       { 'step-component': isVisible }
     ]"
     :style="{ backgroundColor: '#323232', opacity: isVisible ? 1 : 0 }"
