37dd3692f631d55313b61067cfeb9b13da1dcfd7
S1thK3nny <kennethildebrandt@gmail.com>
2024-06-27 12:00:19 +0200

    Improved headline sizes

--- frontend/src/components/Headline.vue
+++ frontend/src/components/Headline.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="styled-text text-left font-bold text-5xl md:text-8xl lg:text-10xl text-shadow-md shadow-black-dark">
+  <p class="styled-text text-left font-bold text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl text-shadow-md shadow-black-dark">
     <slot>Default Text</slot>
   </p>
 </template>
